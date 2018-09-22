73831892fcaf7c0f3c8a97844266fac1ad4438f6
vasanthorapeti <vasanth.horapeti@gmail.com>
2018-09-22 20:59:45 +0530

    New tests

---
+++ UnitTestProject/UnitTest2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //IWebDriver driver = new FirefoxDriver();
+            //driver.Navigate().GoToUrl("https://en.wikipedia.org/wiki/Main_Page");
+            //IWebElement search = driver.FindElement(By.Name("search"));
+            //search.SendKeys("Karnataka");
+
+            string[] lines = { "Bengaluru", "Karnatata", "India" };
+            string result;
+            System.IO.File.WriteAllLines(@"C:\BIECode\TestAutomation\WriteLines.txt", lines);
+            result = "test";
+            Assert.AreEqual("test", result);
+
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest3
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string[] lines = { "Bengaluru", "Karnatata", "India" };
+            string result;
+            System.IO.File.WriteAllLines(@"C:\BIECode\TestAutomation\WriteLines.txt", lines);
+            result = "test";
+            Assert.AreEqual("test", result);
+        }
+    }
+}
