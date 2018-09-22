4476fe1e60902842e7ba11c0cc48cfa5555489fd
vasanthorapeti <vasanth.horapeti@gmail.com>
2018-09-22 12:47:52 +0530

    Add project files.

---
+++ TestAutomation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = { "First line", "Second line", "Third line" };
+            System.IO.File.WriteAllLines(@"C:\BIECode\TestAutomation\WriteLines.txt", lines);
+        }
+    }
+}
