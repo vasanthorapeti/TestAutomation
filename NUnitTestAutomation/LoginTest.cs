40ae77e367c9daa751dbfb738d4fa40a6fd5f7c0
vasanthorapeti <vasanth.horapeti@gmail.com>
2018-09-24 18:06:37 +0530

    t

---
+++ NUnitTestAutomation/LoginTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnitTestAutomation
+{
+    [TestFixture]
+    class LoginTest
+    {
+        [Test]
+        public void LoginCMS1()
+        {
+            string[] lines = { "Bengaluru", "Karnatata", "India" };
+            string result;
+            System.IO.File.WriteAllLines(@"C:\BIECode\TestAutomation\WriteLines.txt", lines);
+            result = "test";
+            Console.WriteLine(TestContext.Parameters["testParameter1"]);
+            Assert.AreEqual("test", result);
+        }        
+    }
+}
